bd893f0be138f4528e6d345806c6c59c6f127daa
lucas abritta <15205208+lucasabritta@users.noreply.github.com>
2025-11-21 17:21:07 +0100

    Create postman collection (#4)

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,19 @@ const app = express();
 app.use(cors()).use(express.json()).options('*', cors());
 
 app.post('/users', async (req: Request, res: Response) => {
-  const user = await User.create(req.body);
+  console.log(req.body);
+  const name = req.body.name;
+
+  if (!name) {
+    return res.status(400).send({ error: 'The attribute "name" is required' });
+  }
+
+  const user = await User.create({ name });
   res.status(201).send(user);
 });
 
 app.get('/users', async (req: Request, res: Response) => {
+  console.log(req.query);
   const users = await User.find().sort({ createdAt: 1 });
   res.status(200).send(users);
 });
