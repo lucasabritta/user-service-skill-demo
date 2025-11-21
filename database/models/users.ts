0142f58bb3a98690075ddb5d4fadf8a0e6b1fdbd
Lucas Abritta <lucasabritta@gmail.com>
2025-11-21 17:40:57 +0100

    Add error handling, the logic for get and post users

--- database/models/users.ts
+++ database/models/users.ts
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true }
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true }
   },
   { timestamps: true }
 );
